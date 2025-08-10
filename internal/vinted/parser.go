7b7fe73abb3b7ee8472eef684f8214a45c6c3a18
Jordan Wyatt <jwyatt1995@gmail.com>
2025-08-10 21:15:22 +0100

    feat: extend url parser package with function to convert SearchParams to API URL

--- internal/vinted/parser.go
+++ internal/vinted/parser.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/url"
 	"strconv"
+	"strings"
 )
 
 type SearchParams struct {
@@ -119,6 +120,74 @@ func ParseVintedURL(u string) (*SearchParams, error) {
 	}
 
 	return params, nil
+}
+
+// https://www.vinted.co.uk/api/v2/catalog/items?page=1&per_page=96&time=1754854403&gen_session_id=true&search_text=universal+works+men&catalog_ids=2051&price_from=0&price_to=100&currency=GBP&order=newest_first&size_ids=209&brand_ids=378695&status_ids=6&patterns_ids=28
+func (s *SearchParams) ToApiURL() (string, error) {
+	baseURL := "https://www.vinted.co.uk/api/v2/catalog/items"
+	values := url.Values{}
+
+	if s.SearchText == "" {
+		return "", fmt.Errorf("missing required parameter: search_text")
+	}
+
+	values.Set("search_text", strings.ReplaceAll(s.SearchText, " ", "+"))
+
+	if s.Currency != "" {
+		values.Set("currency", s.Currency)
+	}
+
+	if s.Currency != "" {
+		values.Set("currency", s.Currency)
+	}
+
+	if s.Order != "" {
+		values.Set("order", s.Order)
+	}
+
+	if s.Time != 0 {
+		values.Set("time", fmt.Sprintf("%d", s.Time))
+	}
+
+	if s.Page != 0 {
+		values.Set("page", fmt.Sprintf("%d", s.Page))
+	}
+
+	if s.PriceFrom != 0 {
+		values.Set("price_from", fmt.Sprintf("%.2f", s.PriceFrom))
+	}
+
+	if s.PriceTo != 0 {
+		values.Set("price_to", fmt.Sprintf("%.2f", s.PriceTo))
+	}
+
+	if len(s.CatalogIDs) > 0 {
+		for _, id := range s.CatalogIDs {
+			values.Add("catalog_ids[]", fmt.Sprintf("%d", id))
+		}
+	}
+	if len(s.SizeIDs) > 0 {
+		for _, id := range s.SizeIDs {
+			values.Add("size_ids[]", fmt.Sprintf("%d", id))
+		}
+	}
+	if len(s.BrandIDs) > 0 {
+		for _, id := range s.BrandIDs {
+			values.Add("brand_ids[]", fmt.Sprintf("%d", id))
+		}
+	}
+	if len(s.StatusIDs) > 0 {
+		for _, id := range s.StatusIDs {
+			values.Add("status_ids[]", fmt.Sprintf("%d", id))
+		}
+	}
+	if len(s.PatternsIDs) > 0 {
+		for _, id := range s.PatternsIDs {
+			values.Add("patterns_ids[]", fmt.Sprintf("%d", id))
+		}
+	}
+
+	return fmt.Sprintf("%s?%s", baseURL, values.Encode()), nil
 }
 
 func convertCommaSeparatedToIntSlice(input []string) ([]int, error) {
--- internal/vinted/parser_test.go
+++ internal/vinted/parser_test.go
@@ -1,23 +1,25 @@
 package internal
 
 import (
+	"net/url"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func Test_Parse_WithFullVintedURL(t *testing.T) {
-	url := "https://www.vinted.co.uk/catalog?search_text=universal%20works%20men&time=1754854403&catalog[]=2051&page=1&size_ids[]=209&brand_ids[]=378695&status_ids[]=6&patterns_ids[]=28&price_from=0&currency=GBP&price_to=100&order=newest_first"
+	url := "https://www.vinted.co.uk/catalog?search_text=universal%20works%20men&time=1754854403&catalog[]=2051&catalog[]=2052&page=1&size_ids[]=209&size_ids[]=210&brand_ids[]=378695&brand_ids[]=378696&status_ids[]=6&status_ids[]=7&patterns_ids[]=28&patterns_ids[]=29&price_from=0&currency=GBP&price_to=100&order=newest_first"
 
 	expected := &SearchParams{
 		SearchText:  "universal works men",
 		Time:        1754854403,
-		CatalogIDs:  []int{2051},
+		CatalogIDs:  []int{2051, 2052},
 		Page:        1,
-		SizeIDs:     []int{209},
-		BrandIDs:    []int{378695},
-		StatusIDs:   []int{6},
-		PatternsIDs: []int{28},
+		SizeIDs:     []int{209, 210},
+		BrandIDs:    []int{378695, 378696},
+		StatusIDs:   []int{6, 7},
+		PatternsIDs: []int{28, 29},
 		PriceFrom:   0,
 		PriceTo:     100,
 		Currency:    "GBP",
@@ -42,4 +44,44 @@ func Test_Parse_MinimalURL(t *testing.T) {
 	require.NoError(t, err, "should not return an error for a valid URL")
 
 	require.Equal(t, expected, actual, "parsed parameters should match expected values")
+}
+
+func Test_ToApiURL_WithCompleteSearchTerms(t *testing.T) {
+	params := &SearchParams{
+		SearchText:  "universal works men",
+		Time:        1754854403,
+		CatalogIDs:  []int{2051, 2052},
+		Page:        1,
+		SizeIDs:     []int{209, 210},
+		BrandIDs:    []int{378695, 378696},
+		StatusIDs:   []int{6, 7},
+		PatternsIDs: []int{28, 29},
+		PriceFrom:   1,
+		PriceTo:     100,
+		Currency:    "GBP",
+		Order:       "newest_first",
+	}
+
+	expectedURL := "https://www.vinted.co.uk/api/v2/catalog/items?brand_ids[]=378695&brand_ids[]=378696&catalog_ids[]=2051&catalog_ids[]=2052&currency=GBP&order=newest_first&page=1&patterns_ids[]=28&patterns_ids[]=29&price_from=1.00&price_to=100.00&search_text=universal+works+men&size_ids[]=209&size_ids[]=210&status_ids[]=6&status_ids[]=7&time=1754854403"
+	actualURL, err := params.ToApiURL()
+	escapedActualURL, err := url.QueryUnescape(actualURL)
+
+	require.NoError(t, err, "should not return an error for valid parameters")
+
+	assert.Equal(t, expectedURL, escapedActualURL, "generated API URL should match expected URL")
+}
+
+func Test_ToApiURL_WithMinimalSearchTerms(t *testing.T) {
+	params := &SearchParams{
+		SearchText: "universal works",
+		Time:       1754855320,
+	}
+
+	expectedURL := "https://www.vinted.co.uk/api/v2/catalog/items?search_text=universal+works&time=1754855320"
+	actualURL, err := params.ToApiURL()
+	escapedActualURL, err := url.QueryUnescape(actualURL)
+
+	require.NoError(t, err, "should not return an error for valid parameters")
+
+	assert.Equal(t, expectedURL, escapedActualURL, "generated API URL should match expected URL")
 }
